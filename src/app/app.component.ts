54ee3721572044fd7b411d0c2ce4217e25d83b04
“Firdaus” <firdaussamad94@gmail.com>
2023-09-15 15:14:49 +0800

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
 import { Component } from "@angular/core";
 import {UsbDriver, WebPrintDriver} from '../../projects/ng-thermal-print/src/lib/drivers';
 import {PrintService} from '../../projects/ng-thermal-print/src/lib/ng-thermal-print.service';
+import {PrintDriver} from '../../projects/ng-thermal-print/src/lib/drivers/PrintDriver';
 
 @Component({
   selector: "app-root",
@@ -12,48 +13,40 @@ export class AppComponent {
   status: boolean = false;
   usbPrintDriver: UsbDriver;
   webPrintDriver: WebPrintDriver;
-  ip: string = "10.83.118.160";
-  testresult: any;
+  ip: string = '';
+  driverTest: PrintDriver;
 
   constructor(private printService: PrintService) {
     this.usbPrintDriver = new UsbDriver();
-
-    this.printService.isConnected.subscribe((result) => {
+    this.printService.isConnected.subscribe(result => {
       this.status = result;
       if (result) {
-
-       alert("Connected to printer!!!");
+        console.log('Connected to printer!!!');
       } else {
-        alert("Not connected to printer.");
+        console.log('Not connected to printer.');
       }
     });
   }
 
   requestUsb() {
-    this.usbPrintDriver.requestUsb().subscribe(
-      (result) => {
-        this.testresult =  result;
-        this.printService.setDriver(result);
-      },
-      (error) => {
-        alert(error);
-      }
-    );
+    this.usbPrintDriver.requestUsb().subscribe(result => {
+      this.printService.setDriver(this.usbPrintDriver, 'ESC/POS');
+    });
   }
 
   connectToWebPrint() {
     this.webPrintDriver = new WebPrintDriver(this.ip);
-    this.printService.setDriver(this.webPrintDriver, "WebPRNT");
+    this.printService.setDriver(this.webPrintDriver, 'WebPRNT');
   }
 
-  print() {
-    this.printService
-      .init()
+  print(driver?: PrintDriver) {
+    this.driverTest = driver;
+    this.printService.init()
       .setBold(true)
-      .writeLine("Hello World!")
+      .writeLine('Hello World!')
       .setBold(false)
       .feed(4)
-      .cut("full")
+      .cut('full')
       .flush();
   }
 }
